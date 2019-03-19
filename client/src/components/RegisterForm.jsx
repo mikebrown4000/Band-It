f8d86d8560079795d3a0173862bb84248c645ebe
Luane Souza <luanesouza@ool-ad020729.dyn.optonline.net>
2019-03-19 18:17:40 -0400

    changed style of registerForm

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -18,13 +18,55 @@ function RegisterForm(props) {
   return(
     <div>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} type='text' placeholder='First Name' name='first_name' value={first_name} />
-        <input onChange={handleChange} type='text' placeholder='Last Name' name='last_name' value={last_name} />
-        <input onChange={handleChange} type='text' placeholder='Email' name='email' value={email} />
-        <input onChange={handleChange} type='password' placeholder='Password' name='password' value={password} />
-        <input onChange={handleChange} type='text' placeholder='Location' name='location' value={location} />
-        <input onChange={handleChange} type='text' placeholder='Instrument' name='instrument' value={instrument} />
-        <input onChange={handleChange} type='number' placeholder='Age' name='age' value={age} />
+        <input
+        onChange={handleChange}
+        type='text'
+        placeholder='First Name'
+        name='first_name'
+        value={first_name} />
+
+        <input
+        onChange={handleChange}
+        type='text'
+        placeholder='Last Name'
+        name='last_name'
+        value={last_name} />
+
+        <input
+        onChange={handleChange}
+        type='text'
+        placeholder='Email'
+        name='email'
+        value={email} />
+
+        <input
+        onChange={handleChange}
+        type='password'
+        placeholder='Password'
+        name='password'
+        value={password} />
+
+        <input
+        onChange={handleChange}
+        type='text'
+        placeholder='Location'
+        name='location'
+        value={location} />
+
+        <input
+        onChange={handleChange}
+        type='text'
+        placeholder='Instrument'
+        name='instrument'
+        value={instrument} />
+
+        <input
+        onChange={handleChange}
+        type='number'
+        placeholder='Age'
+        name='age'
+        value={age} />
+
         <label htmlFor='looking'>Looking For Band? </label>
         <input onClick={handleCheck} type='checkbox' name='looking' value={looking} />
         <input value='CREATE ACCOUNT' type='submit'/>
