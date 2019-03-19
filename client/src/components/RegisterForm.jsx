3391db1a5828f216b04cac179570233c7cc8ab15
Luane Souza <luanesouza@ool-ad020729.dyn.optonline.net>
2019-03-19 18:12:59 -0400

    changed input type of password to passowrd instead of text

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -21,7 +21,7 @@ function RegisterForm(props) {
         <input onChange={handleChange} type='text' placeholder='First Name' name='first_name' value={first_name} />
         <input onChange={handleChange} type='text' placeholder='Last Name' name='last_name' value={last_name} />
         <input onChange={handleChange} type='text' placeholder='Email' name='email' value={email} />
-        <input onChange={handleChange} type='text' placeholder='Password' name='password' value={password} />
+        <input onChange={handleChange} type='password' placeholder='Password' name='password' value={password} />
         <input onChange={handleChange} type='text' placeholder='Location' name='location' value={location} />
         <input onChange={handleChange} type='text' placeholder='Instrument' name='instrument' value={instrument} />
         <input onChange={handleChange} type='number' placeholder='Age' name='age' value={age} />
