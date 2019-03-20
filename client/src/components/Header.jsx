103141ad08b4837eb47e3cdefb52340a2f0c1a47
James Yun <jamesyun@ool-ad0205ef.dyn.optonline.net>
2019-03-20 15:56:15 -0400

    styled

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,16 +4,20 @@ import { Link } from 'react-router-dom';
 function Header(props) {
   const { handleLogout } = props
   return(
-    <div className='main-header'>
-      <h1>BandIt</h1>
-      <nav className='main-nav'>
-        <Link to='/'>Home</Link>
-        <Link to='/artists'>Artists</Link>
-        <Link to='/bands'>Bands</Link>
-        <Link to ='/createband'>Create your band</Link>
-        <Link to ='/createmusician'>Create a Musician</Link>
-        <button onClick={handleLogout}>Log Out</button>
-      </nav>
+    <div className='whole-div-header'>
+      <header className='main-header'>
+        <nav className='main-nav'>
+          <Link to='/'>Home</Link>
+          <Link to='/artists'>Artists</Link>
+          <Link to='/bands'>Bands</Link>
+          <Link to ='/createband'>Create your band</Link>
+          <Link to ='/createmusician'>Create a Musician</Link>
+          <button id='logout-button' onClick={handleLogout}>Log Out</button>
+        </nav>
+        <div>
+          <h1>Band-It</h1>
+        </div>
+      </header>
     </div>
   )
 }
