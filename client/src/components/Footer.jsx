24e0456a473a3fd83a46b5234fb659639aa85351
Luane Souza <luanesouza@ool-ad020729.dyn.optonline.net>
2019-03-21 20:42:21 -0400

    fixed the link in the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default () => {
   return(
   <footer>
       <p>Copyright Sad Pandas &copy;</p>
-      <a href={'https://git.generalassemb.ly/wdi-nyc-bananas/auth-dragon'}>
+      <a href={'https://github.com/mikebrown4000/Band-It'}>
         <img src={'https://image.flaticon.com/icons/svg/25/25231.svg'} width={40} alt="image" />
       </a>
   </footer>
