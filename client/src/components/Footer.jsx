aee0f8030e2ffba31e005430e8532f3999cc006a
Luane Souza <luanesouza@ool-ad020729.dyn.optonline.net>
2019-03-21 20:28:49 -0400

    added footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return(
   <footer>
-      <p>Copyright Bananas General Assembly &copy;</p>
+      <p>Copyright Sad Pandas &copy;</p>
       <a href={'https://git.generalassemb.ly/wdi-nyc-bananas/auth-dragon'}>
         <img src={'https://image.flaticon.com/icons/svg/25/25231.svg'} width={40} alt="image" />
       </a>
